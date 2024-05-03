4116c4ea8c3976a0d68c97f20167d67ca23795f2
Anna <aelise17264@gmail.com>
2024-05-02 19:55:06 -0600

    block validation with tests

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -86,15 +86,48 @@ class Block:
             return last_block.difficulty - 1
 
         return 1
+    
+    @staticmethod
+    def is_valid_block(last_block, block):
+
+        if block.last_hash != last_block.hash:
+            raise Exception('The block last_hash must be correct')
+        
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('The proof of work requirement was not met')
+        
+        if abs(last_block.difficulty - block.difficulty) > 1:
+            raise Exception('The block difficulty must only adjust by 1')
+        
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+
+        if block.hash != reconstructed_hash:
+            raise Exception('The block has must be correct')
+
+
 
 def main():
+    genesis_block = Block.genesis()
+    bad_block = Block.mine_block(genesis_block, 'start')
+    # bad_block.last_hash = 'problem_child'
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+        print('success')
+    except Exception as e:
+        print(f'is valid block: {e}')
     # block = Block('blockparty')
     # print(block)
     # print(f'block.py __name__: {__name__}')
 
-    genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'start')
-    print(block)
+    # genesis_block = Block.genesis()
+    # block = Block.mine_block(genesis_block, 'start')
+    # print(block)
 
 if __name__ == "__main__":
     main()
