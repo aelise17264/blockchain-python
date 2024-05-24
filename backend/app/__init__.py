b561b1c4249cab6d5ea310178f0a4bbfa3189352
Anna <aelise17264@gmail.com>
2024-05-24 11:19:22 -0600

    initial routes to GET data from the blockchain

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
+blockchain = Blockchain()
+
+# adding temp code to view on our running app
+for i in range(3):
+    blockchain.add_block(i)
 
 # endpoints
 
 @app.route('/')
 def default():
     return 'Welcome to the Blockchain'
+
+
+@app.route('/blockchain')
+def route_blockchain():
+    # cannot just return an instance of the chain because of its data type
+    # instead return the helper function that stringifies the chain for us
+    return blockchain.__repr__()
+
 
 app.run()
